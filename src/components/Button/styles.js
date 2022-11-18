33f0114b7ccee31cd6b79289bda979a84cdd5d38
Alexsander Furtado <alexsander.f.lex@gmail.com>
2022-11-18 03:18:30 -0300

    calculadora react

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 4px solid #000000;
+    background-color: #7790ed;
+    color: #000000;
+    border-radius: 8px;
+    font-size: 32px;
+    font-weight: 700;
+    flex: 1;
+    
+    &:hover {
+        opacity: 0.1;
+    }
+`
